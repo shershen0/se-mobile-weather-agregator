fc469ec1218529f7671d00fcda392e1d3838040f
Arsenii Pimenov <parseny@mail.ru>
2022-11-30 15:10:06 +0300

    changed request to api

--- app/src/main/java/hse/ru/se_mobile_weather_agregator/MainActivity.kt
+++ app/src/main/java/hse/ru/se_mobile_weather_agregator/MainActivity.kt
@@ -1,33 +1,30 @@
 package hse.ru.se_mobile_weather_agregator
 
-import com.android.volley.RequestQueue
-import com.android.volley.toolbox.StringRequest
-
 //import hse.ru.weather.databinding.ActivityMainBinding
 import android.app.Notification
 import android.app.NotificationManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import hse.ru.se_mobile_weather_agregator.databinding.ActivityMainBinding
 import okhttp3.*
 import java.io.IOException
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.File
-import android.util.Log
-import androidx.databinding.DataBindingUtil
-import com.android.volley.toolbox.Volley
-import okhttp3.*
-import com.google.gson.Gson;
-import  com.android.volley.Request
 
 class MainActivity : AppCompatActivity() {
     private val API_KEY = "dc142bca70ce4a92bca105853221910"
     lateinit var binding: ActivityMainBinding
     lateinit var itemModel: WeatherModel
+    var requestQueue: RequestQueue? = null
 
     data class WeatherModel(
         val temperature: String,
@@ -36,43 +33,21 @@ class MainActivity : AppCompatActivity() {
         val condition: String
     )
 
-    var requestQueue: RequestQueue? = null
-
-    val key = "7ab76c22a12f41ff929105944223011"
-
-    private fun getUrl(city: String): String {
-        return "https://api.weatherapi.com/v1/forecast.json?key=" +
-                key +
-                "&q=" +
-                city +
-                "&days=" +
-                "1" +
-                "&aqi=no&alerts=no"
+    override fun onStart() {
+        super.onStart()
+        requestQueue = Volley.newRequestQueue(this)
+        requestQueue!!.add(jsonArrayRequest)
+        Log.i("onStart", "initialize VOLLEY queue")
     }
-
-    private val jsonArrayRequest = StringRequest(
-        Request.Method.GET,
-        getUrl("Saint-Petersburg"),
-        { response ->
-            print("Successfully")
-            Log.d("Volley on response", response)
-        },
-        { error ->
-            print(error)
-        }
-    )
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        binding = ActivityMainBinding.inflate(layoutInflater, R.layout.activity_main)
-//        setContentView(binding.root)
-//        binding.activity = this
+        binding = ActivityMainBinding.inflate(layoutInflater, R.layout.activity_main)
+        setContentView(binding.root)
+        binding.activity = this
 
 //        var button: Button = findViewById(R.id.button1)
 //        button.setOnClickListener{
@@ -107,6 +82,28 @@ class MainActivity : AppCompatActivity() {
         weather.text = itemModel.condition
     }
 
+    private fun getUrl(city: String): String {
+        return "https://api.weatherapi.com/v1/forecast.json?key=" +
+                API_KEY +
+                "&q=" +
+                city +
+                "&days=" +
+                "1" +
+                "&aqi=no&alerts=no"
+    }
+
+    private val jsonArrayRequest = StringRequest(
+        com.android.volley.Request.Method.GET,
+        getUrl("Saint-Petersburg"),
+        { response ->
+            print("Successfully")
+            Log.d("Volley on response", response)
+        },
+        { error ->
+            print(error)
+        }
+    )
+
     private fun parseWeatherData(result: String) {
         val mainObject = JSONObject(result)
         val item = WeatherModel(
@@ -116,20 +113,10 @@ class MainActivity : AppCompatActivity() {
             mainObject.getJSONObject("current").getJSONObject("condition").getString("text")
         )
 
-
-    }
-
-    override fun onStart() {
-        super.onStart()
-        requestQueue = Volley.newRequestQueue(this)
-//        requestQueue!!.add(jsonArrayRequest)
-        Log.i("onStart", "initialize VOLLEY queue")
     }
 
     fun sendRequest() {
         requestQueue!!.add(jsonArrayRequest)
 
     }
-
-
 }
